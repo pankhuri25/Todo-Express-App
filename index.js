606f66007a13e0e6a4123858f4960423dcacb72c
Pankhuri <pmn.bhatagar@gmail.com>
2022-07-19 01:19:22 +0530

    added view engine

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const port = 7000;
 
 const app =  express();
 
+app.use('/', require('./routes'));
 
-
+// ${} is used for interpolating values directly. Use backticks to use them.
 app.listen(port, (err)=>{
     if(err){
         console.log(`Error in running Server: ${err}`);
